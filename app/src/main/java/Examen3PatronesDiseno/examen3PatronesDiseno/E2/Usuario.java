b6170f6310c1328980fae1beb8f5c3761836cd7c
Katzumi Urdininea <katurdininea02@gmail.com>
2023-03-23 19:03:23 -0400

    Commit #1 (OS_id)

---
+++ app/src/main/java/Examen3PatronesDiseno/examen3PatronesDiseno/E2/Usuario.java
@@ -0,0 +1,29 @@
+package Examen3PatronesDiseno.examen3PatronesDiseno.E2;
+
+public class Usuario {
+    private int id;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    private String nombre;
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    private String password;
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    publ
+    
+    
+}
